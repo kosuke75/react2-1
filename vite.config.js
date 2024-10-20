7ee2571d50920e52e0435e81a72c85251f09dec4
kosuke75 <kousuke_2527.1@icloud.com>
2024-10-20 19:24:43 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default {
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react/jsx-runtime']
+      external: ['react/jsx-runtime'] 
     }
   }
-}
+});
